c0a6a96f7719f2377feedc93051b92f07ea96951
svenreyes <svenxx64@gmail.com>
2026-04-14 15:13:14 -0400

    new skulpted

--- src/features/skulpted/components/index.ts
+++ src/features/skulpted/components/index.ts
@@ -1,2 +1,4 @@
 export { BlurSlideshow } from "./BlurSlideshow";
+export { default as EufolioLogo } from "./EufolioLogo";
+export { default as FinexaLogo } from "./FinexaLogo";
 
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,3 +1,8 @@
 /// <reference types="vite/client" />
 /// <reference types="react" />
 /// <reference types="react-dom" />
+
+declare module '*.pdf' {
+  const src: string;
+  export default src;
+}
